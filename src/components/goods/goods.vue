201d8e44b6a2484cdaca0389cc451ead68a3f861
hurui007 <hurui_h@qq.com>
2019-08-15 23:32:03 +0800

    完整菜单列表和food列表的联动和滚动效果

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="goods">
-      <div class="menu-wrapper">
+      <div class="menu-wrapper" ref="menuWrapper">
         <ul>
           <!--eslint-disable-next-line-->
-          <li v-for="item in goods" class="menu-item">
+          <li v-for="(item,index) in goods" class="menu-item" v-bind:class="{'current':currentIndex === index}" @click="selectMenu(index,$event)">
             <span class="text border-1px">
               <span v-show="item.type > 0" class="icon" v-bind:class="classMap[item.type]">
               </span>
@@ -12,10 +12,10 @@
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper">
+      <div class="foods-wrapper" ref="foodsWrapper">
         <ul>
           <!--eslint-disable-next-line-->
-          <li v-for="item in goods" class="food-list">
+          <li v-for="item in goods" class="food-list food-list-hook">
             <h1 class="title">{{item.name}}</h1>
             <!-- 每个分类有多个商品 -->
             <ul>
@@ -28,12 +28,10 @@
                   <h2 class="name">{{food.name}}</h2>
                   <p class="desc">{{food.description}}</p>
                   <div class="extra">
-                    <span class="count">月售{{food.sellCount}}份</span>
-                    <span>好评率{{food.rating}}</span>
+                    <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}</span>
                   </div>
                   <div class="price">
-                    <span class="now">{{food.price}}</span>
-                    <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
+                    <span class="now">{{food.price}}</span><span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
                   </div>
                 </div>
               </li>
@@ -45,6 +43,9 @@
 </template>
 
 <script>
+
+  import BScroll from 'better-scroll'
+
   const ERR_OK = 0
   export default {
     name: 'goods',
@@ -55,7 +56,22 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
+      }
+    },
+    computed: {
+      // 计算属性,计算滚动位置
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          if (!height2 || (this.scrollY >= height && this.scrollY < height2)) {
+            return i
+          }
+        }
+        return 0
       }
     },
     created () {
@@ -64,8 +80,49 @@
         res = res.body
         if (res.errno === ERR_OK) {
           this.goods = res.data
+          this.$nextTick(() => {
+            // $nextTick 这个方法可以保证在数据变化后更新dom
+            // 在这里添加滚动条
+            this._initScroll()
+            // 左侧根据右侧滚动进行高亮
+            this._calculateHeight()
+          })
         }
       })
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true // 可以添加点击效果
+        })
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3
+        })
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight() {
+        // 根据类名找到这个元素
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          // 每个分类对应的dom高度
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
+      selectMenu(index, event) {
+        // 点击左侧菜单，右侧滚动到指定位置
+        if (!event._constructed) {
+          return
+        }
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        let el = foodList[index]
+        this.foodsScroll.scrollToElement(el, 300)
+      }
     }
   }
 </script>
@@ -90,6 +147,16 @@
         width: 56px
         line-height: 14px
         padding: 0 12px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            font-size: 16px
+            line-height: 16px
+            border-none()
         .icon
           display: inline-block
           vertical-align: top
@@ -139,18 +206,20 @@
           flex: 1
           .name
             margin: 2px 0 8px 0
-            height: 14px
+            height: 18px
             line-height: 14px
-            font-size: 14px
+            font-size: 16px
             color: rgb(7,17,27)
           .desc,.extra
-            line-height: 10px
-            font-size: 10px
+            line-height: 12px
+            font-size: 14px
+            margin-top: 4px
             color: rgb(147,153,159)
           .desc
             margin-bottom: 8px
+            line-height: 12px
           .extra
-            &.count
+            .count
               margin-right: 12px
           .price
             font-weight: 700
